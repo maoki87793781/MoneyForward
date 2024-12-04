b02a9e39a336685e2c5791b6261e3ddb7deba8e1
maoki87793781 <87636477+maoki87793781@users.noreply.github.com>
2024-12-04 20:57:20 +0900

    Add files via upload

--- MoneyForward/MoneyForwardMe_3_DataEntry.py
+++ MoneyForward/MoneyForwardMe_3_DataEntry.py
@@ -34,7 +34,7 @@ records = worksheet.get_all_records()
 df = pd.DataFrame(records).head()
 
 # データフレームを表示
-print(df)
+#print(df)
 
 #2. MoneyForward ME にログインする
 # WebDriverの設定と起動
@@ -113,46 +113,213 @@ try:
 
     #3. 家計簿データを MoneyForward ME に登録する
     for index, row in df.iterrows():
-        print(row['振替'])        
-        print(row['日付'])
-        print(row['内容'])
-        print(row['金額（円）'])    
-        print(row['大項目'])
-        print(row['中項目'])
-        print(row['メモ'])                            
+        time.sleep(2)        
         # 手入力ボタンをクリック
         input_button = wait.until(
             EC.element_to_be_clickable((By.XPATH, '//*[@id="kakeibo"]/section/div[1]/div[1]/div/button'))
         )
         input_button.click()
 
-        # 日付を入力
-        date_input = WebDriverWait(chrome_driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, '//*[@id="updated-at"]'))
-        )
-        time.sleep(5)
-        date_input.send_keys(row['日付'])
-        time.sleep(5)
-        # 支払金額を入力
-        amount_input = WebDriverWait(chrome_driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, '//*[@id="appendedPrependedInput"]'))
-        )
-        amount_input.send_keys(row['金額（円）'])
+        if row['振替'] == 1:
+            # 振替タブをクリック
+            transfer_tab = wait.until(
+                EC.element_to_be_clickable((By.XPATH, '//*[@id="success"]'))
+            )
+            transfer_tab.click()
+            print("振替の入力")
+            print(row['振替'])        
+            print(row['日付'])
+            print(row['内容'])
+            print(row['金額（円）'])    
+            print(row['大項目'])
+            print(row['中項目'])
+            print(row['メモ'])                            
 
-        # メモを入力
-        memo_input = WebDriverWait(chrome_driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, '//*[@id="js-content-field"]'))
-        )
-        memo_input.send_keys(str(row['メモ'])) 
+            # 日付を入力
+            date_input = wait.until(
+                EC.element_to_be_clickable((By.XPATH, '//*[@id="updated-at"]'))
+            )
+            time.sleep(2)
+            date_input.clear()
+            date_input.send_keys(row['日付'])
+            time.sleep(2)
+            # 振替金額を入力
+            amount_input = wait.until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="appendedPrependedInput"]'))
+            )
+            amount_input.send_keys(row['金額（円）'])
 
-    # 登録ボタンをクリック
-    save_button = WebDriverWait(chrome_driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, '//*[@id="submit-button"]'))
-    )
-    save_button.click()
+            # 項目を入力
+            large_category_select = wait.until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="js-large-category-selected"]'))
+            )
+            large_category_option = large_category_select.find_element(By.XPATH, f'//option[text()="{row["大項目"]}"]')
+            large_category_option.click()
 
+            # 内容を入力
+            memo_input = wait.until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="js-content-field"]'))
+            )
+            memo_input.send_keys(str(row['内容'])) 
+
+            # 登録ボタンをクリック
+            save_button = wait.until(
+                EC.element_to_be_clickable((By.XPATH, '//*[@id="submit-button"]'))
+            )
+            save_button.click()
+
+            # 続けて入力ボタンをクリック
+            coninue_button = wait.until(
+                EC.element_to_be_clickable((By.XPATH, '//*[@id="confirmation-button"]'))
+            )
+            coninue_button.click()
+            time.sleep(2)
+            print("続けて入力ボタン成功！")
+
+            # closeボタンをクリック
+            close_button = wait.until(
+                EC.element_to_be_clickable((By.XPATH, '//*[@id="form-user-asset-act"]/div[1]/div'))
+            )
+            close_button.click()     
+
+        elif row['金額（円）'] < 0:
+            # 支出タブをクリック
+            expense_tab = wait.until(
+                EC.element_to_be_clickable((By.XPATH, '//*[@id="important"]'))
+            )
+            expense_tab.click()
+            print("支出の入力")
+            print(row['振替'])        
+            print(row['日付'])
+            print(row['内容'])
+            print(row['金額（円）'])    
+            print(row['大項目'])
+            print(row['中項目'])
+            print(row['メモ'])                            
+
+            # 日付を入力
+            date_input = WebDriverWait(chrome_driver, 10).until(
+                EC.element_to_be_clickable((By.XPATH, '//*[@id="updated-at"]'))
+            )
+            time.sleep(2)
+            date_input.clear()
+            date_input.send_keys(row['日付'])
+            time.sleep(2)
+            # 支払金額を入力
+            amount_input = WebDriverWait(chrome_driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="appendedPrependedInput"]'))
+            )
+            #マイナスは入力できないのでプラスに変換
+            amount =row['金額（円）'] * -1
+            amount_input.send_keys(amount)
+
+            # 項目を入力
+            large_category_select = wait.until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="js-large-category-selected"]'))
+            )
+            large_category_option = large_category_select.find_element(By.XPATH, f'//option[text()="{row["大項目"]}"]')
+            large_category_option.click()
+
+            # 内容を入力
+            memo_input = WebDriverWait(chrome_driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="js-content-field"]'))
+            )
+            memo_input.send_keys(str(row['内容'])) 
+
+            # 登録ボタンをクリック
+            save_button = WebDriverWait(chrome_driver, 10).until(
+                EC.element_to_be_clickable((By.XPATH, '//*[@id="submit-button"]'))
+            )
+            save_button.click()
+
+            # 続けて入力ボタンをクリック
+            coninue_button = WebDriverWait(chrome_driver, 10).until(
+                EC.element_to_be_clickable((By.XPATH, '//*[@id="confirmation-button"]'))
+            )
+            coninue_button.click()
+            time.sleep(2)
+            print("続けて入力ボタン成功！")
+
+            # closeボタンをクリック
+            close_button = WebDriverWait(chrome_driver, 10).until(
+                EC.element_to_be_clickable((By.XPATH, '//*[@id="form-user-asset-act"]/div[1]/div'))
+            )
+            close_button.click()        
+
+        else:
+            # 収入タブをクリック
+            income_tab = wait.until(
+                EC.element_to_be_clickable((By.XPATH, '//*[@id="info"]'))
+            )
+            income_tab.click()
+            print("収入の入力")
+            print(row['振替'])        
+            print(row['日付'])
+            print(row['内容'])
+            print(row['金額（円）'])    
+            print(row['大項目'])
+            print(row['中項目'])
+            print(row['メモ'])                            
+
+            # 日付を入力
+            date_input = wait.until(
+                EC.element_to_be_clickable((By.XPATH, '//*[@id="updated-at"]'))
+            )
+            time.sleep(2)
+            date_input.clear()
+            date_input.send_keys(row['日付'])
+            time.sleep(2)
+            # 収入金額を入力
+            amount_input = wait.until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="appendedPrependedInput"]'))
+            )
+            amount_input.send_keys(row['金額（円）'])
+
+            # 項目を入力
+            # ①要素をクリック
+            time.sleep(2)            
+            large_category_select = wait.until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="js-large-category-selected"]')))
+            large_category_select.click()  # クリックして選択肢を表示
+            if row['大項目'] == "通信費":
+                large_category_option = large_category_select.find_element(By.XPATH, '//*[@id="6"]')
+                large_category_option.click()
+
+            elif row['大項目'] == "食費":
+                large_category_option = large_category_select.find_element(By.XPATH, '//*[@id="11"]')
+                large_category_option.click()
+            else:
+                large_category_option = large_category_select.find_element(By.XPATH, '//*[@id="11"]')
+                large_category_option.click()
+
+            # 内容を入力
+            memo_input = wait.until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="js-content-field"]'))
+            )
+            memo_input.send_keys(str(row['内容'])) 
+
+            # 登録ボタンをクリック
+            save_button = wait.until(
+                EC.element_to_be_clickable((By.XPATH, '//*[@id="submit-button"]'))
+            )
+            save_button.click()
+
+            # 続けて入力ボタンをクリック
+            coninue_button = wait.until(
+                EC.element_to_be_clickable((By.XPATH, '//*[@id="confirmation-button"]'))
+            )
+            coninue_button.click()
+            time.sleep(2)
+            print("続けて入力ボタン成功！")
+
+            # closeボタンをクリック
+            close_button = wait.until(
+                EC.element_to_be_clickable((By.XPATH, '//*[@id="form-user-asset-act"]/div[1]/div'))
+            )
+            close_button.click()          
+        
     # 登録完了まで待機
-    time.sleep(1000)
+    time.sleep(2)
 
 
 except Exception as e:
@@ -162,13 +329,4 @@ finally:
     # ブラウザを閉じる
     chrome_driver.quit()
     print("ブラウザを終了しました。")
-
-
-
-        # 手入力ボタンをクリック
-#        input_button = wait.until(
-#            EC.element_to_be_clickable((By.XPATH, ’//*[@id="kakeibo"]’))
-#        )
-
-#        input_button.click()
 
